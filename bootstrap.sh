6d973010a9a8ddf2147f42d99b7907899fb8eab9
Martin Salles <martin.salles@free.fr>
2015-05-15 20:46:26 +0200

    Set up repo with Vagrant django config

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Update package list and upgrade all packages
+sudo apt-get update
+
+
+# Install python pip3
+sudo apt-get -y install python-pip
+
+# Install django package
+sudo pip install django
+
+# Install Django REST Framework
+sudo pip install djangorestframework
+sudo pip install markdown
+sudo pip install django-filter
+
+# Install Django migration tool
+sudo pip install south
+
+# Install debug tools
+sudo apt-get -y install curl
